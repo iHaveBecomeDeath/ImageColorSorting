4d2f08d8023c116e3404173e205eb5d9e968e085
Kalle Wallin <kalle.wallin@gmail.com>
2014-02-24 11:40:17 +0100

    180-ish named html colors added for definition and matching

---
+++ js/colors.js
@@ -0,0 +1,567 @@
+var colors = 
+{
+  IndianRed {
+    name: "IndianRed",
+    hex: "CD5C5C"
+  },
+  LightCoral {
+    name: "LightCoral",
+    hex: "F08080"
+  },
+  Salmon {
+    name: "Salmon",
+    hex: "FA8072"
+  },
+  DarkSalmon {
+    name: "DarkSalmon",
+    hex: "E9967A"
+  },
+  LightSalmon {
+    name: "LightSalmon",
+    hex: "FFA07A"
+  },
+  Crimson {
+    name: "Crimson",
+    hex: "DC143C"
+  },
+  Red {
+    name: "Red",
+    hex: "FF0000"
+  },
+  FireBrick {
+    name: "FireBrick",
+    hex: "B22222"
+  },
+  DarkRed {
+    name: "DarkRed",
+    hex: "8B0000"
+  },
+  Pink {
+    name: "Pink",
+    hex: "FFC0CB"
+  },
+  LightPink {
+    name: "LightPink",
+    hex: "FFB6C1"
+  },
+  HotPink {
+    name: "HotPink",
+    hex: "FF69B4"
+  },
+  DeepPink {
+    name: "DeepPink",
+    hex: "FF1493"
+  },
+  MediumVioletRed {
+    name: "MediumVioletRed",
+    hex: "C71585"
+  },
+  PaleVioletRed {
+    name: "PaleVioletRed",
+    hex: "DB7093"
+  },
+  Coral {
+    name: "Coral",
+    hex: "FF7F50"
+  },
+  Tomato {
+    name: "Tomato",
+    hex: "FF6347"
+  },
+  OrangeRed {
+    name: "OrangeRed",
+    hex: "FF4500"
+  },
+  DarkOrange {
+    name: "DarkOrange",
+    hex: "FF8C00"
+  },
+  Orange {
+    name: "Orange",
+    hex: "FFA500"
+  },
+  Gold {
+    name: "Gold",
+    hex: "FFD700"
+  },
+  Yellow {
+    name: "Yellow",
+    hex: "FFFF00"
+  },
+  LightYellow {
+    name: "LightYellow",
+    hex: "FFFFE0"
+  },
+  LemonChiffon {
+    name: "LemonChiffon",
+    hex: "FFFACD"
+  },
+  LightGoldenrodYellow {
+    name: "LightGoldenrodYellow",
+    hex: "FAFAD2"
+  },
+  PapayaWhip {
+    name: "PapayaWhip",
+    hex: "FFEFD5"
+  },
+  Moccasin {
+    name: "Moccasin",
+    hex: "FFE4B5"
+  },
+  PeachPuff {
+    name: "PeachPuff",
+    hex: "FFDAB9"
+  },
+  PaleGoldenrod {
+    name: "PaleGoldenrod",
+    hex: "EEE8AA"
+  },
+  Khaki {
+    name: "Khaki",
+    hex: "F0E68C"
+  },
+  DarkKhaki {
+    name: "DarkKhaki",
+    hex: "BDB76B"
+  },
+  Lavender {
+    name: "Lavender",
+    hex: "E6E6FA"
+  },
+  Thistle {
+    name: "Thistle",
+    hex: "D8BFD8"
+  },
+  Plum {
+    name: "Plum",
+    hex: "DDA0DD"
+  },
+  Violet {
+    name: "Violet",
+    hex: "EE82EE"
+  },
+  Orchid {
+    name: "Orchid",
+    hex: "DA70D6"
+  },
+  Fuchsia {
+    name: "Fuchsia",
+    hex: "FF00FF"
+  },
+  Magenta {
+    name: "Magenta",
+    hex: "FF00FF"
+  },
+  MediumOrchid {
+    name: "MediumOrchid",
+    hex: "BA55D3"
+  },
+  MediumPurple {
+    name: "MediumPurple",
+    hex: "9370DB"
+  },
+  Amethyst {
+    name: "Amethyst",
+    hex: "9966CC"
+  },
+  BlueViolet {
+    name: "BlueViolet",
+    hex: "8A2BE2"
+  },
+  DarkViolet {
+    name: "DarkViolet",
+    hex: "9400D3"
+  },
+  DarkOrchid {
+    name: "DarkOrchid",
+    hex: "9932CC"
+  },
+  DarkMagenta {
+    name: "DarkMagenta",
+    hex: "8B008B"
+  },
+  Purple {
+    name: "Purple",
+    hex: "800080"
+  },
+  Indigo {
+    name: "Indigo",
+    hex: "4B0082"
+  },
+  SlateBlue {
+    name: "SlateBlue",
+    hex: "6A5ACD"
+  },
+  DarkSlateBlue {
+    name: "DarkSlateBlue",
+    hex: "483D8B"
+  },
+  MediumSlateBlue {
+    name: "MediumSlateBlue",
+    hex: "7B68EE"
+  },
+  GreenYellow {
+    name: "GreenYellow",
+    hex: "ADFF2F"
+  },
+  Chartreuse {
+    name: "Chartreuse",
+    hex: "7FFF00"
+  },
+  LawnGreen {
+    name: "LawnGreen",
+    hex: "7CFC00"
+  },
+  Lime {
+    name: "Lime",
+    hex: "00FF00"
+  },
+  LimeGreen {
+    name: "LimeGreen",
+    hex: "32CD32"
+  },
+  PaleGreen {
+    name: "PaleGreen",
+    hex: "98FB98"
+  },
+  LightGreen {
+    name: "LightGreen",
+    hex: "90EE90"
+  },
+  MediumSpringGreen {
+    name: "MediumSpringGreen",
+    hex: "00FA9A"
+  },
+  SpringGreen {
+    name: "SpringGreen",
+    hex: "00FF7F"
+  },
+  MediumSeaGreen {
+    name: "MediumSeaGreen",
+    hex: "3CB371"
+  },
+  SeaGreen {
+    name: "SeaGreen",
+    hex: "2E8B57"
+  },
+  ForestGreen {
+    name: "ForestGreen",
+    hex: "228B22"
+  },
+  Green {
+    name: "Green",
+    hex: "008000"
+  },
+  DarkGreen {
+    name: "DarkGreen",
+    hex: "006400"
+  },
+  YellowGreen {
+    name: "YellowGreen",
+    hex: "9ACD32"
+  },
+  OliveDrab {
+    name: "OliveDrab",
+    hex: "6B8E23"
+  },
+  Olive {
+    name: "Olive",
+    hex: "808000"
+  },
+  DarkOliveGreen {
+    name: "DarkOliveGreen",
+    hex: "556B2F"
+  },
+  MediumAquamarine {
+    name: "MediumAquamarine",
+    hex: "66CDAA"
+  },
+  DarkSeaGreen {
+    name: "DarkSeaGreen",
+    hex: "8FBC8F"
+  },
+  LightSeaGreen {
+    name: "LightSeaGreen",
+    hex: "20B2AA"
+  },
+  DarkCyan {
+    name: "DarkCyan",
+    hex: "008B8B"
+  },
+  Teal {
+    name: "Teal",
+    hex: "008080"
+  },
+  Aqua {
+    name: "Aqua",
+    hex: "00FFFF"
+  },
+  Cyan {
+    name: "Cyan",
+    hex: "00FFFF"
+  },
+  LightCyan {
+    name: "LightCyan",
+    hex: "E0FFFF"
+  },
+  PaleTurquoise {
+    name: "PaleTurquoise",
+    hex: "AFEEEE"
+  },
+  Aquamarine {
+    name: "Aquamarine",
+    hex: "7FFFD4"
+  },
+  Turquoise {
+    name: "Turquoise",
+    hex: "40E0D0"
+  },
+  MediumTurquoise {
+    name: "MediumTurquoise",
+    hex: "48D1CC"
+  },
+  DarkTurquoise {
+    name: "DarkTurquoise",
+    hex: "00CED1"
+  },
+  CadetBlue {
+    name: "CadetBlue",
+    hex: "5F9EA0"
+  },
+  SteelBlue {
+    name: "SteelBlue",
+    hex: "4682B4"
+  },
+  LightSteelBlue {
+    name: "LightSteelBlue",
+    hex: "B0C4DE"
+  },
+  PowderBlue {
+    name: "PowderBlue",
+    hex: "B0E0E6"
+  },
+  LightBlue {
+    name: "LightBlue",
+    hex: "ADD8E6"
+  },
+  SkyBlue {
+    name: "SkyBlue",
+    hex: "87CEEB"
+  },
+  LightSkyBlue {
+    name: "LightSkyBlue",
+    hex: "87CEFA"
+  },
+  DeepSkyBlue {
+    name: "DeepSkyBlue",
+    hex: "00BFFF"
+  },
+  DodgerBlue {
+    name: "DodgerBlue",
+    hex: "1E90FF"
+  },
+  CornflowerBlue {
+    name: "CornflowerBlue",
+    hex: "6495ED"
+  },
+  RoyalBlue {
+    name: "RoyalBlue",
+    hex: "4169E1"
+  },
+  Blue {
+    name: "Blue",
+    hex: "0000FF"
+  },
+  MediumBlue {
+    name: "MediumBlue",
+    hex: "0000CD"
+  },
+  DarkBlue {
+    name: "DarkBlue",
+    hex: "00008B"
+  },
+  Navy {
+    name: "Navy",
+    hex: "000080"
+  },
+  MidnightBlue {
+    name: "MidnightBlue",
+    hex: "191970"
+  },
+  Cornsilk {
+    name: "Cornsilk",
+    hex: "FFF8DC"
+  },
+  BlanchedAlmond {
+    name: "BlanchedAlmond",
+    hex: "FFEBCD"
+  },
+  Bisque {
+    name: "Bisque",
+    hex: "FFE4C4"
+  },
+  NavajoWhite {
+    name: "NavajoWhite",
+    hex: "FFDEAD"
+  },
+  Wheat {
+    name: "Wheat",
+    hex: "F5DEB3"
+  },
+  BurlyWood {
+    name: "BurlyWood",
+    hex: "DEB887"
+  },
+  Tan {
+    name: "Tan",
+    hex: "D2B48C"
+  },
+  RosyBrown {
+    name: "RosyBrown",
+    hex: "BC8F8F"
+  },
+  SandyBrown {
+    name: "SandyBrown",
+    hex: "F4A460"
+  },
+  Goldenrod {
+    name: "Goldenrod",
+    hex: "DAA520"
+  },
+  DarkGoldenrod {
+    name: "DarkGoldenrod",
+    hex: "B8860B"
+  },
+  Peru {
+    name: "Peru",
+    hex: "CD853F"
+  },
+  Chocolate {
+    name: "Chocolate",
+    hex: "D2691E"
+  },
+  SaddleBrown {
+    name: "SaddleBrown",
+    hex: "8B4513"
+  },
+  Sienna {
+    name: "Sienna",
+    hex: "A0522D"
+  },
+  Brown {
+    name: "Brown",
+    hex: "A52A2A"
+  },
+  Maroon {
+    name: "Maroon",
+    hex: "800000"
+  },
+  White {
+    name: "White",
+    hex: "FFFFFF"
+  },
+  Snow {
+    name: "Snow",
+    hex: "FFFAFA"
+  },
+  Honeydew {
+    name: "Honeydew",
+    hex: "F0FFF0"
+  },
+  MintCream {
+    name: "MintCream",
+    hex: "F5FFFA"
+  },
+  Azure {
+    name: "Azure",
+    hex: "F0FFFF"
+  },
+  AliceBlue {
+    name: "AliceBlue",
+    hex: "F0F8FF"
+  },
+  GhostWhite {
+    name: "GhostWhite",
+    hex: "F8F8FF"
+  },
+  WhiteSmoke {
+    name: "WhiteSmoke",
+    hex: "F5F5F5"
+  },
+  Seashell {
+    name: "Seashell",
+    hex: "FFF5EE"
+  },
+  Beige {
+    name: "Beige",
+    hex: "F5F5DC"
+  },
+  OldLace {
+    name: "OldLace",
+    hex: "FDF5E6"
+  },
+  FloralWhite {
+    name: "FloralWhite",
+    hex: "FFFAF0"
+  },
+  Ivory {
+    name: "Ivory",
+    hex: "FFFFF0"
+  },
+  AntiqueWhite {
+    name: "AntiqueWhite",
+    hex: "FAEBD7"
+  },
+  Linen {
+    name: "Linen",
+    hex: "FAF0E6"
+  },
+  LavenderBlush {
+    name: "LavenderBlush",
+    hex: "FFF0F5"
+  },
+  MistyRose {
+    name: "MistyRose",
+    hex: "FFE4E1"
+  },
+  Gainsboro {
+    name: "Gainsboro",
+    hex: "DCDCDC"
+  },
+  LightGrey {
+    name: "LightGrey",
+    hex: "D3D3D3"
+  },
+  Silver {
+    name: "Silver",
+    hex: "C0C0C0"
+  },
+  DarkGray {
+    name: "DarkGray",
+    hex: "A9A9A9"
+  },
+  Gray {
+    name: "Gray",
+    hex: "808080"
+  },
+  DimGray {
+    name: "DimGray",
+    hex: "696969"
+  },
+  LightSlateGray {
+    name: "LightSlateGray",
+    hex: "778899"
+  },
+  SlateGray {
+    name: "SlateGray",
+    hex: "708090"
+  },
+  DarkSlateGray {
+    name: "DarkSlateGray",
+    hex: "2F4F4F"
+  },
+  Black {
+    name: "Black",
+    hex: "000000"
+  }
+}
